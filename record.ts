ed554d9afa2ff0599e5a950817a4c62fd115dbb7
MX Development <37406242+ElMuchoLoco@users.noreply.github.com>
2023-04-17 17:12:30 +0100

    Incorporating All Config

---
+++ record.ts
@@ -0,0 +1,11 @@
+interface Entry {
+    id: number;
+    name: string;
+    artist: string;
+    songname: string;
+    youtubeURL: string;
+
+}
+
+const dataList: Array<Entry> | [] undefined = [];
+
